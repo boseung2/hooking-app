833308090fe14e0ab2497e8990d3df2b202c04c7
boseung2 <jungbs3726@naver.com>
2023-11-15 22:37:27 +0900

    Feat: header Authorization 토큰 추가

--- src/apollo/createApolloClient.ts
+++ src/apollo/createApolloClient.ts
@@ -1,8 +1,41 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, HttpLink, from } from "@apollo/client";
 import { createApolloCache } from "./createApolloCache";
+import { onError } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.log(`[GraphQL error]: -> ${operation.operationName}
+    Message: ${message}, Query: ${path}, Location: ${JSON.stringify(
+        locations
+      )}`);
+    });
+  }
+
+  if (networkError) {
+    console.log(`[networkError]: -> ${operation.operationName}
+    Message: ${networkError.message}
+    `);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((request, prevContext) => {
+  const accessToken = localStorage.getItem("access_token");
+  return {
+    headers: {
+      ...prevContext.headers,
+      Authorization: accessToken ? `Bearer ${accessToken}` : "",
+    },
+  };
+});
 
 export const createApolloClient = () =>
   new ApolloClient({
-    uri: "http://localhost:4000/graphql",
     cache: createApolloCache(),
+    link: from([authLink, errorLink, httpLink]),
   });
